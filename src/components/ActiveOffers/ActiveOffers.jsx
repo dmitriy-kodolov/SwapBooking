f20818ef83ba56297afc2496f833114780b1ddef
Дмитрий Кодолов <dim.kodolov@yandex.ru>
2021-10-20 20:02:22 +0400

    SB-93 изменил логику всвязи изменениями  бэкэнда

--- src/components/ActiveOffers/ActiveOffers.jsx
+++ src/components/ActiveOffers/ActiveOffers.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-duplicate-case */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-vars */
 import { makeStyles } from '@material-ui/styles';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
@@ -27,31 +28,68 @@ const useStyle = makeStyles({
     margin: '15px',
   },
 });
+
 const ActiveOffers = () => {
   const userId = useSelector(((state) => state.login.userId));
   const style = useStyle();
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
+  const [masOfIdExchange, setMasOfIdExchange] = useState([]);
+  const [exchange, setExchange] = useState([]);
   const [isAcceptUserStepOne, setIsAcceptUserStepOne] = useState(false);
   const [isAcceptUserStepTwo, setIsAcceptUserStepTwo] = useState(false);
   const [isAcceptUserStepThree, setIsAcceptUserStepThree] = useState(false);
   const [isAcceptContrUserStepOne, setIsAcceptContrUserStepOne] = useState(false);
   const [isAcceptContrUserStepTwo, setIsAcceptContrUserStepTwo] = useState(false);
   const [isAcceptContrUserStepThree, setIsAcceptContrUserStepThree] = useState(false);
+  // TODO надо добавить изменения шагов контр-агента после доработки бэка
+  // switch (exchange?.OtherBook?.StatusText) {
+  //   case 'Подтверждена. Ожидает трек-номера':
+  //     setIsAcceptContrUserStepOne(true);
+  //     break;
+  //   case 'Книга доставляется. Ожидаем трек-номер':
+  //     setIsAcceptContrUserStepTwo(true);
+  //     break;
+  //   case `Книга ${'dsadsa'} доставлена`:
+  //     setIsAcceptContrUserStepThree(false);
+  //     break;
+  //   default:
+  //     break;
+  // }
 
-  (async () => {
-    await restGet();
-  })();
+  // (async () => {
+  // await restGet(`/api/exchange/${userId}/all`);
+  // })();
   const propsFrom = useForm();
   const {
     handleSubmit, control,
   } = propsFrom;
   const formValues = propsFrom?.getValues();
-
+  // получение  id активных обменов
+  useEffect(() => {
+    restGet(`/api/exchange/${userId}/all`)
+      .then(({ data }) => {
+        setMasOfIdExchange(data);
+      })
+      .catch((err) => alert(`Не удалось загрузить список, ${err.message}`));
+  }, []);
+  if (!masOfIdExchange?.length) {
+    return (<p>У вас нет активного обмена</p>);
+  }
+  // получение конкретного обмена
+  useEffect(() => {
+    restGet(`/api/exchange/${userId}/${'exchangeId'}`)
+      .then(({ data }) => setExchange(data))
+      .catch((err) => alert(`Не удалось получить активный обмен ${err.message}`));
+  }, [masOfIdExchange]);
+  // отпарвка трек номера
   const onSubmitForm = () => {
     console.log(formValues);
     restPost(`/api/exchange/send/${userId}/${'exchangeId'}`, formValues) // надо изменить на exchangeId
       .then(() => {
         setIsAcceptUserStepTwo(true);
+        restGet(`/api/exchange/${userId}/${'exchangeId'}`)
+          .then(({ data }) => setExchange(data))
+          .catch((err) => alert(`Не удалось получить статус обмена ${err.message}`));
         if (isAcceptContrUserStepTwo) {
           setStep((prev) => prev + 1);
         }
@@ -80,6 +118,10 @@ const ActiveOffers = () => {
             <br />
             <Typography align="center" variant="body2">
               Тут должна быть хотелка какая-то
+              {exchange?.OtherBook?.BookName}
+              {exchange?.OtherBook?.Note}
+              {exchange?.OtherBook?.AuthorFirstName}
+              {exchange?.OtherBook?.AuthorLastName}
             </Typography>
             <br />
             <br />
@@ -122,6 +164,10 @@ const ActiveOffers = () => {
             <br />
             <Typography align="center" variant="body2">
               Здесь то что пользователь будет менять:
+              {exchange?.MyBook?.BookName}
+              {exchange?.MyBook?.Note}
+              {exchange?.MyBook?.AuthorFirstName}
+              {exchange?.MyBook?.AuthorLastName}
             </Typography>
             <br />
             <br />
@@ -137,8 +183,11 @@ const ActiveOffers = () => {
                 // здесь отправка подтверждения обмена
                 onClick={() => {
                   restPost(`/api/exchange/agree/${userId}/${'exchangeId'}`) // здесь надо менять на Id обмена
-                    .then(() => {
+                    .then(async () => {
                       setIsAcceptUserStepOne(true);
+                      await restGet(`/api/exchange/${userId}/${'exchangeId'}`) // здесь надо менять на Id обмена
+                        .then(({ data }) => setExchange(data))
+                        .catch((err) => alert(`Не удалось получить активный обмен ${err.message}`));
                       if (isAcceptContrUserStepOne) {
                         setStep((prev) => prev + 1);
                       }
@@ -224,6 +273,16 @@ const ActiveOffers = () => {
           {/* <CardActions style={{ justifyContent: 'center' }} /> */}
         </Card>
       </div>
+      <Button
+        variant="contained"
+        onClick={() => {
+          restGet(`/api/exchange/${userId}/${[...masOfIdExchange]}`) // здесь надо менять на Id обмена
+            .then(({ data }) => setExchange(data))
+            .catch((err) => alert(`Не удалось получить активный обмен ${err.message}`));
+        }}
+      >
+        Обновить статус обмена
+      </Button>
     </div>
   );
 };
